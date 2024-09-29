073519133d968cdf5c2107ef6a64ff15651c79e7
vibha sheladiya <vibhasheladiya3936@gmail.com>
2024-09-29 16:23:05 +0530

    inital

--- src/routes/v1/book.route.js
+++ src/routes/v1/book.route.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const {bookController}=require('../../controllers');
 const { singleFileUpload } = require("../../helpers/upload");
+const { accessToken } = require('../../middlewares/auth');
 
 const router=express.Router();
 
@@ -11,7 +12,7 @@ bookController.createBook
 );
 
 // // list book 
-router.get("/list-book",
+router.get("/list-book",accessToken(),
     bookController.getBookList
     );
 
